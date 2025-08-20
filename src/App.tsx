d24d2a18899e7d13d3bc8627716f5bd63e43957f
Khushi-gorasiya <kgorasiya13@gmail.com>
2025-08-20 23:35:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import PriorityFlag from '../component/PriorityFlag';
+import PriorityFlag from '../component/PriorityFlag.tsx';
 
 function App() {
   const [emailText, setEmailText] = useState('');
