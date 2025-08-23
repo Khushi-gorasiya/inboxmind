00fe5ff457b1776c1a8002693580d14d6818a038
Khushi-gorasiya <kgorasiya13@gmail.com>
2025-08-23 23:15:04 +0530

    Update reply.js

--- api/reply.js
+++ api/reply.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch('https://api-inference.huggingface.co/models/google/flan-t5-large', {
+    const API_URL = 'https://api-inference.huggingface.co/models/google/flan-t5-base', {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${process.env.VITE_HUGGINGFACE_TOKEN}`,
