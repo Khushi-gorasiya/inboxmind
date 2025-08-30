61100105eedb56cacec9ab116ac22aa61699e94c
Khushi-gorasiya <kgorasiya13@gmail.com>
2025-08-30 22:46:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import PriorityFlag from './component/PriorityFlag';
 import SmartReply from './component/SmartReply';
 import EventDetector from './component/EventDetector';
-import SpamDetection from './component/SpamDetector';
+import SpamDetector from './component/SpamDetector';
 
 function App() {
   const [emailText, setEmailText] = useState('');
