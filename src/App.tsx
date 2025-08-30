cf5577b101a3eef03363034dab4311764329f4c8
Khushi-gorasiya <kgorasiya13@gmail.com>
2025-08-30 22:14:49 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import PriorityFlag from './component/PriorityFlag';
 import SmartReply from './component/SmartReply';
 import EventDetector from './component/EventDetector';
-import SpamFlag from './component/SpamFlag';  // Import the new SpamFlag component
+import SpamDetection from './component/SpamDetection';  // Import the SpamDetection component
 
 function App() {
   const [emailText, setEmailText] = useState('');
@@ -121,8 +121,20 @@ function App() {
           {summary}
         </div>
 
+        {/* 🛑 Spam Detection Feature */}
+        <SpamDetection emailText={emailText} /> {/* Add the SpamDetection component here */}
+
         {/* 🚦 Priority Flag */}
         <PriorityFlag emailText={emailText} />
 
         {/* ✍️ Smart Reply Generator */}
-        <SmartReply emailText
+        <SmartReply emailText={emailText} />
+
+        {/* 📅 Event Detector */}
+        <EventDetector emailText={emailText} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
