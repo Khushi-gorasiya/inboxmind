f2109b2345395e17641fec75a35295df40758755
Khushi-gorasiya <kgorasiya13@gmail.com>
2025-09-03 22:55:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import PriorityFlag from './component/PriorityFlag';
 import SmartReply from './component/SmartReply';
 import EventDetector from './component/EventDetector';
 import SpamFlag from './component/SpamFlag';
+import ToneAnalyzer from './component/ToneAnalyzer';
 
 function App() {
   const [emailText, setEmailText] = useState('');
@@ -144,6 +145,7 @@ function App() {
         <SmartReply emailText={debouncedText} />
         <EventDetector emailText={debouncedText} />
         <SpamFlag emailText={debouncedText} />
+        <ToneAnalyzer emailText={debouncedText} />
       </div>
     </div>
   );
