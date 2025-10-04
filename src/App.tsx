872f5c07357bf3013d51716150eb628e30b28313
Khushi-gorasiya <kgorasiya13@gmail.com>
2025-10-04 23:10:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import PriorityFlag from './component/PriorityFlag';
 import SmartReply from './component/SmartReply';
-import EventDetector from './component/EventDetector';
+// import EventDetector from './component/EventDetector';
 import SpamFlag from './component/SpamFlag';
 import ToneAnalyzer from './component/ToneAnalyzer';
 import FollowUpReminder from './component/FollowUpReminder';
@@ -159,7 +159,7 @@ function App() {
         <div style={{ marginTop: '2rem' }}>
           <PriorityFlag emailText={debouncedText} />
           <SmartReply emailText={debouncedText} />
-          <EventDetector emailText={debouncedText} />  
+          {/* <EventDetector emailText={debouncedText} />  */}
           <SpamFlag emailText={debouncedText} />
           <ToneAnalyzer emailText={debouncedText} />
           <FollowUpReminder emailText={debouncedText} />
